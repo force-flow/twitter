d1372ece5e4b313ef084335cca89608f39979291
force-flow <33775978+force-flow@users.noreply.github.com>
2019-06-05 21:42:09 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,22 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body class="bg-light">
-	<div class="container-fluid">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, odio! Id consectetur officia eum cupiditate voluptates consequatur quasi voluptas distinctio, vitae iste asperiores similique doloremque quia delectus tempore sequi. Quos.</div>
+	<div class="container-fluid bg-white">
+	<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul>	
+	</div>
 	<div class="container">
 		<div class="row">
 		<div class="col-3">
@@ -125,7 +140,7 @@
 	<img src="" alt="">
 		<div class="col-3">
 			<!--Кого читать-->
-			<div class="bg-white pl-3">
+			<div class="bg-white pl-3 border">
 				<div class="row">
 					<div class="col-6">
 						<h5>Кого читать</h5>
